65bae5da1d0d001c5e52d90ced840b6c463dd316
leblancr <rich.leblanc1@proton.me>
2024-07-23 19:19:52 -0400

    finished part 3

--- django_tutorial/polls/urls.py
+++ django_tutorial/polls/urls.py
@@ -20,7 +20,18 @@ Naming your URL lets you refer to it unambiguously from elsewhere in Django, esp
 This powerful feature allows you to make global changes to the URL patterns of your project while only touching a single file.
 """
 
+app_name = "polls"
+
 # path to function map
+# 1st arg = everything after http://127.0.0.1:8000/polls/
+# 2nd arg = function in polls/views.py
 urlpatterns = [
-    path("", views.index, name="index"),  # path after http://127.0.0.1:8000/polls/
+    # ex: /polls/
+    path("", views.index, name="index"),
+    # ex: /polls/5/
+    path("<int:question_id>/", views.detail, name="detail"),
+    # ex: /polls/5/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ex: /polls/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
