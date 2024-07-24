dea515cd2316c969750af40f4e9f847ebf5b3017
leblancr <rich.leblanc1@proton.me>
2024-07-23 22:06:18 -0400

    finished part 4

--- django_tutorial/polls/urls.py
+++ django_tutorial/polls/urls.py
@@ -26,12 +26,8 @@ app_name = "polls"
 # 1st arg = everything after http://127.0.0.1:8000/polls/
 # 2nd arg = function in polls/views.py
 urlpatterns = [
-    # ex: /polls/
-    path("", views.index, name="index"),
-    # ex: /polls/5/
-    path("<int:question_id>/", views.detail, name="detail"),
-    # ex: /polls/5/results/
-    path("<int:question_id>/results/", views.results, name="results"),
-    # ex: /polls/5/vote/
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
