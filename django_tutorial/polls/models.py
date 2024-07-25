d2b5a781e94be0587f250f2b79bd7605c77d33f1
leblancr <rich.leblanc1@proton.me>
2024-07-25 11:57:14 -0400

    finished part 5 testing

--- django_tutorial/polls/models.py
+++ django_tutorial/polls/models.py
@@ -11,10 +11,14 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
+    #  each Choice object has only one choice text.
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    # question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='choices')
+
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
